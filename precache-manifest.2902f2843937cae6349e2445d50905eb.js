dcdf6ae22a26029d63be6d2bc32fa9ba93d722df
@amankakar <amankhan1017@gmail.com>
2021-01-15 12:23:42 +0500

    Updates

---
+++ precache-manifest.2902f2843937cae6349e2445d50905eb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "be469c1d679765803399",
+    "url": "/deploye_Escrow/static/css/main.5f7c5154.chunk.css"
+  },
+  {
+    "revision": "be469c1d679765803399",
+    "url": "/deploye_Escrow/static/js/main.be469c1d.chunk.js"
+  },
+  {
+    "revision": "fecad157cdbd3c3a25d9",
+    "url": "/deploye_Escrow/static/js/runtime~main.fecad157.js"
+  },
+  {
+    "revision": "d668d5fc645b6dfe4600",
+    "url": "/deploye_Escrow/static/css/2.ea4b964b.chunk.css"
+  },
+  {
+    "revision": "d668d5fc645b6dfe4600",
+    "url": "/deploye_Escrow/static/js/2.d668d5fc.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/deploye_Escrow/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/deploye_Escrow/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/deploye_Escrow/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/deploye_Escrow/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/deploye_Escrow/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "37db7165040316fb99f5f128914e2d93",
+    "url": "/deploye_Escrow/index.html"
+  }
+];
